ebee5962cb0fad1c6f629183a43aeb6f396dc575
Tadeusz Kleszcz <tkleszcz@silenteight.com>
2022-11-02 19:34:06 +0100

    add storage and atlantis accounts

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,61 @@ module "sandbox90" {
 
   account_customizations_name = "sandbox"
 }
+
+module "storage1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "tadeuszkleszcz+storage1@gmail.com"
+    AccountName               = "storage1"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "tadeuszkleszcz+storage1@gmail.com"
+    SSOUserFirstName          = "storage1"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+    date = "2022-10-27"
+  }
+
+  account_customizations_name = "storage"
+}
+
+module "atlantis1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "tadeuszkleszcz+atlantis1@gmail.com"
+    AccountName               = "atlantis1"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "tadeuszkleszcz+atlantis1@gmail.com"
+    SSOUserFirstName          = "atlantis1"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+    date = "2022-10-27"
+  }
+
+  account_customizations_name = "atlantis"
+}
