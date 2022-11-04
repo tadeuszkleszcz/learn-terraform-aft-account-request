293f986ebc1d7dd91f2f5fa24f69f67bc69b4d3a
Tadeusz Kleszcz <tkleszcz@silenteight.com>
2022-11-04 11:30:57 +0100

    storage4 account request

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,12 @@
-module "sandbox99" {
+module "storage01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "tadeuszkleszcz+sandbox99@gmail.com"
-    AccountName               = "sandbox99"
+    AccountEmail              = "tadeuszkleszcz+storage01@gmail.com"
+    AccountName               = "storage01"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "tadeuszkleszcz+sandbox99@gmail.com"
-    SSOUserFirstName          = "sandbox99"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-    date = "2022-10-27"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-module "storage2" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "tadeuszkleszcz+storage2@gmail.com"
-    AccountName               = "storage2"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "tadeuszkleszcz+storage2@gmail.com"
-    SSOUserFirstName          = "storage2"
+    SSOUserEmail              = "tadeuszkleszcz+storage01@gmail.com"
+    SSOUserFirstName          = "storage01"
     SSOUserLastName           = "AFT"
   }
 
@@ -55,62 +26,3 @@ module "storage2" {
 
   account_customizations_name = "storage"
 }
-
-module "storage3" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "tadeuszkleszcz+storage3@gmail.com"
-    AccountName               = "storage3"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "tadeuszkleszcz+storage3@gmail.com"
-    SSOUserFirstName          = "storage3"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-    date = "2022-10-27"
-  }
-
-  account_customizations_name = "storage"
-}
-
-module "storage4" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "tadeuszkleszcz+storage4@gmail.com"
-    AccountName               = "storage4"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "tadeuszkleszcz+storage4@gmail.com"
-    SSOUserFirstName          = "storage4"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-    date = "2022-10-27"
-  }
-
-  account_customizations_name = "storage"
-}
-
