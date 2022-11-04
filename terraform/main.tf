273be9cc3182bc0fd2a9ef4edcafe2f5a6da589c
Tadeusz Kleszcz <tkleszcz@silenteight.com>
2022-11-04 10:11:27 +0100

    add new acc

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,3 +55,32 @@ module "storage2" {
 
   account_customizations_name = "storage"
 }
+
+module "storage3" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "tadeuszkleszcz+storage3@gmail.com"
+    AccountName               = "storage3"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "tadeuszkleszcz+storage3@gmail.com"
+    SSOUserFirstName          = "storage3"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+    date = "2022-10-27"
+  }
+
+  account_customizations_name = "storage"
+}
