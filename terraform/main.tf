1d13bac378f5e2373f9ce96e4f115dca0dd98619
Tadeusz Kleszcz <tkleszcz@silenteight.com>
2022-11-08 19:24:10 +0100

    add StorageDemo02

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,32 @@ module "StorageDemo01" {
 
   account_customizations_name = "storage"
 }
+
+module "StorageDemo02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "tadeuszkleszcz+StorageDemo02@gmail.com"
+    AccountName               = "StorageDemo02"
+    ManagedOrganizationalUnit = "AFT Demo"
+    SSOUserEmail              = "tadeuszkleszcz+StorageDemo02@gmail.com"
+    SSOUserFirstName          = "Storage"
+    SSOUserLastName           = "Demo 02"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+    date = "2022-10-27"
+  }
+
+  account_customizations_name = "storage"
+}
